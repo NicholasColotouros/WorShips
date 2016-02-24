d80a5f35bedb286d3acefa9e0bbb1152b38781b7
SamsonStannus <samsonstannus@gmail.com>
2016-02-23 23:21:21 -0500

    added PlayerOne and PlayerTwo to ThirdpersonCPP file with animations

---
+++ Plugins/OceanPlugin/Intermediate/Build/Mac/UE4Editor/Development/OceanPlugin/OceanPluginPrivatePCH.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_OCEANPLUGINPRIVATEPCH_H__
+#define __AUTO_OCEANPLUGINPRIVATEPCH_H__
+//Last Write: 2016-02-21 10:10:07 PM +00:00
+#include "/Volumes/LaCie/UbisoftComp/Branches/WarShips/Plugins/OceanPlugin/Source/OceanPlugin/Classes/OceanPluginPrivatePCH.h"
+#endif//__AUTO_OCEANPLUGINPRIVATEPCH_H__
