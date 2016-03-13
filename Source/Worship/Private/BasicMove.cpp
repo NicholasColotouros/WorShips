42da96607c70239621750b30fe4a14dccd40d95a
Philippe Tremblay <phil.tremblay.tri@gmail.com>
2016-03-12 19:45:16 -0500

    MOVING AI!!

---
+++ Source/Worship/Private/BasicMove.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Worship.h"
+#include "BasicMove.h"
+#include "WorshipBoatAIController.h"
+#include "WorshipBoat.h"
+
+/* AI Module includes */
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+/* This contains includes all key types like UBlackboardKeyType_Vector used below. */
+#include "BehaviorTree/Blackboard/BlackboardKeyAllTypes.h"
+
+EBTNodeResult::Type UBasicMove::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	AWorshipBoatAIController* CurrentController = Cast<AWorshipBoatAIController>(OwnerComp.GetAIOwner());
+	if (CurrentController == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+	AWorshipBoat* CurrentPawn = Cast<AWorshipBoat>(CurrentController->GetControlledPawn());
+	if (CurrentPawn == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+	for (size_t i = 0; i < 5; i++)
+	{
+		CurrentPawn->GearUp();
+	}
+	/*APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(),0);
+	if (PlayerPawn == nullptr)
+	{
+		return EBTNodeResult
+	}*/
+		
+	return EBTNodeResult::Succeeded;
+}
