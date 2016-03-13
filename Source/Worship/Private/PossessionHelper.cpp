004041308dcdea1c49954d73e22343326e0c2963
David Blader <dblader79@gmail.com>
2016-03-12 19:39:46 -0500

    added linear level

---
+++ Source/Worship/Private/PossessionHelper.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Worship.h"
+#include "PossessionHelper.h"
+
+UFUNCTION(BlueprintCallable, Category = "Possession")
+static void setPossessionPawn(AActor possessable, AActor character)
+{
+	
+}
---
+++ Source/Worship/Public/WorshipBallista.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "WorshipVehicle.h"
+#include "WorshipBallista.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class WORSHIP_API AWorshipBallista : public AWorshipVehicle
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = Controls)
+		void Fire();
+
+
+
+};
