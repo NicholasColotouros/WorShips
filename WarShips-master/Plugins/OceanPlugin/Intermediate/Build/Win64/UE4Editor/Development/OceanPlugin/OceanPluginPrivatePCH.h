f0f9f96c0da4367c5035ed2c2e8aa6a800fd72bd
Nicholas Nathan Colotouros <agentnnc@gmail.com>
2016-03-14 11:24:35 -0400

    Deleted duplicate project contained within the map import.

--- WarShips-master/Plugins/OceanPlugin/Intermediate/Build/Win64/UE4Editor/Development/OceanPlugin/OceanPluginPrivatePCH.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_OCEANPLUGINPRIVATEPCH_H__
-#define __AUTO_OCEANPLUGINPRIVATEPCH_H__
-//Last Write: 2/12/2016 10:39:58 PM +00:00
-#include "C:\Users\David\Documents\Unreal Projects\WarShips-master\Plugins\OceanPlugin\Source\OceanPlugin\Classes\OceanPluginPrivatePCH.h"
-#endif//__AUTO_OCEANPLUGINPRIVATEPCH_H__
